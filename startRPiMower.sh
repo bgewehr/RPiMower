3994a1e246cfbc42946e702f2b500c660483dfbc
root <root@RPiMower.(none)>
2015-09-20 06:53:31 +0000

    started with mapping

--- startRPiMower.sh
+++ startRPiMower.sh
@@ -1,4 +1,7 @@
-sudo python ./RPiMower.py
+sudo python ./sens_frontSonar.py &
+sudo python ./sens_groundCam.py &
+sudo python ./sens_compass.py &
+sudo python ./RPiMower.py &
 
 read -n1 -rsp "Press any key to continue... "
 sudo killall python
